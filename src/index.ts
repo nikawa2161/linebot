a516b4be915cf90c63b1477754da86a2dfb1f589
nikawa2161 <nikawa.2161.t@gmail.com>
2024-06-16 11:10:10 +0900

    feat: line setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+  LINE_CHANNEL_ACCESS_TOKEN: string;
+  LINE_CHANNEL_SECRET: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
