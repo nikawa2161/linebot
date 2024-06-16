9a8529d65b59e68635f90d45a4045c3dffbff7c7
nikawa2161 <nikawa.2161.t@gmail.com>
2024-06-16 10:32:54 +0900

    firstcommit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/hello', (c) => {
+  return c.json({
+    ok: true,
+    message: 'Hello Hono!',
+  })
+})
+
+export default app
